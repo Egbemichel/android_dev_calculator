ee6e6253acd4fa26affee6a91698a068c5342a73
Egbe-michel <149802747+Egbemichel@users.noreply.github.com>
2026-03-08 23:23:11 +0100

    learn: test oop knowledge with exercises from lecture notes as well updating the grade calculator

---
+++ Exercise_01/interface_drawable.dart
@@ -0,0 +1,35 @@
+﻿/// Exercise 3: Interface Drawable
+abstract class Drawable {
+  void draw();
+}
+class Circle implements Drawable {
+  final int radius;
+  Circle(this.radius);
+  @override
+  void draw() {
+    print('Circle (radius: \):');
+    print('  ***  ');
+    print(' ***** ');
+    print('*******');
+    print(' ***** ');
+    print('  ***  ');
+  }
+}
+class Square implements Drawable {
+  final int sideLength;
+  Square(this.sideLength);
+  @override
+  void draw() {
+    print('Square (side: \):');
+    print('**********');
+    print('*        *');
+    print('*        *');
+    print('*        *');
+    print('**********');
+  }
+}
+void main() {
+  print('\nEXERCISE 3: Interface - Drawable\n');
+  final shapes = <Drawable>[Circle(3), Square(5)];
+  shapes.forEach((s) => s.draw());
+}
